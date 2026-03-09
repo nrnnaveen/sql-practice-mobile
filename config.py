5b95aeefb390ce693fbcc85b866e0ab4f6109917
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-03-10 00:34:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+MYSQL_CONFIG = {
+"host":"localhost",
+"user":"root",
+"password":"",
+"database":"practice_db"
+}
+
+POSTGRES_CONFIG = {
+"host":"localhost",
+"user":"postgres",
+"password":"",
+"dbname":"practice_db"
+}
