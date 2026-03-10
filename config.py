90f1190224f07b9c5c09415d48037e3859d9d953
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-03-10 08:45:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
-SECRET_KEY = "your-secret-key"
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
 
 MYSQL_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "password",
-    "database": "your_mysql_db"
+    "host": os.environ.get("MYSQL_HOST", "localhost"),
+    "user": os.environ.get("MYSQL_USER", "root"),
+    "password": os.environ.get("MYSQL_PASSWORD", ""),
+    "database": os.environ.get("MYSQL_DATABASE", "test_db")
 }
 
 POSTGRES_CONFIG = {
-    "host": "localhost",
-    "user": "postgres",
-    "password": "password",
-    "database": "your_postgres_db"
+    "host": os.environ.get("POSTGRES_HOST", "localhost"),
+    "user": os.environ.get("POSTGRES_USER", "postgres"),
+    "password": os.environ.get("POSTGRES_PASSWORD", ""),
+    "database": os.environ.get("POSTGRES_DATABASE", "test_db")
 }
