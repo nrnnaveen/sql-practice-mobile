e8711c903a3e850810b3313b894185d52d4c8ab0
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-03-10 10:33:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,17 @@
 import os
-import secrets
 
-# -----------------------------
-# Flask secret key
-# -----------------------------
-# Use environment variable if provided, else generate a random key
-SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_hex(16)
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
 
-# -----------------------------
-# MySQL config (PlanetScale / cloud)
-# -----------------------------
 MYSQL_CONFIG = {
-    "host": os.environ.get("MYSQL_HOST", "your_mysql_host_here"),
-    "user": os.environ.get("MYSQL_USER", "your_mysql_user_here"),
-    "password": os.environ.get("MYSQL_PASSWORD", "your_mysql_password_here"),
-    "database": os.environ.get("MYSQL_DATABASE", "your_mysql_database_here"),
-    "ssl_verify_cert": True  # Required for PlanetScale SSL
+    "host": os.environ.get("MYSQL_HOST", "localhost"),
+    "user": os.environ.get("MYSQL_USER", "root"),
+    "password": os.environ.get("MYSQL_PASSWORD", ""),
+    "database": os.environ.get("MYSQL_DATABASE", "test_db")
 }
 
-# -----------------------------
-# PostgreSQL config (ElephantSQL / cloud)
-# -----------------------------
 POSTGRES_CONFIG = {
-    "host": os.environ.get("POSTGRES_HOST", "your_postgres_host_here"),
-    "user": os.environ.get("POSTGRES_USER", "your_postgres_user_here"),
-    "password": os.environ.get("POSTGRES_PASSWORD", "your_postgres_password_here"),
-    "database": os.environ.get("POSTGRES_DATABASE", "your_postgres_database_here"),
-    "sslmode": "require"  # Required for cloud PostgreSQL
+    "host": os.environ.get("POSTGRES_HOST", "localhost"),
+    "user": os.environ.get("POSTGRES_USER", "postgres"),
+    "password": os.environ.get("POSTGRES_PASSWORD", ""),
+    "database": os.environ.get("POSTGRES_DATABASE", "test_db")
 }
